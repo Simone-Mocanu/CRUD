ab3c8a0ba6505d35e981e77b5c86b8e27e261c97
beji <simone0443@outlook.com>
2024-04-23 21:56:10 +0300

    first stable commit

--- connect.php
+++ connect.php
@@ -10,4 +10,4 @@ $con = mysqli_connect($servername, $username, $password, $dbname);
 
 if (!$con) {
     echo "Conectarea la baza de date a esuat!";
-}
+} 
--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<? 
+<?php
 include 'connect.php';
 $query = 'SELECT * FROM studenti';
 $result = mysqli_query($con, $query);
@@ -20,7 +20,7 @@ if(mysqli_num_rows($result) == 0) {
 } else {
 ?>
 
-<table>
+<table border="1">
     <tr>
         <th>Nume</th>
         <th>Prenume</th>
@@ -37,17 +37,22 @@ if(mysqli_num_rows($result) == 0) {
     ?>
 
     <tr>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
+        <td><?php echo $row['nume']?></td>
+        <td><?php echo $row['prenume']?></td>
+        <td><?php echo $row['email']?></td>
+        <td><?php echo $row['telefon']?></td>
+        <td><?php echo $row['data_nastere']?></td>
+        <td><?php echo $row['adresa']?></td>
+        <td><?php echo $row['oras']?></td>
+        <td><?php echo $row['tara']?></td>
+        <td><?php echo $row['cod_postal']?></td>
+        <td><a href="edit.php?id=<?php echo $row['id'];?>" title="Editeaza">Editeaza</a></td>
+        <td><a href="delete.php?id=<?php echo $row['id'];?>" title="Sterge" onclick="return confirm('Sigur vrei sa stergi?')">Sterge</a></td>
     </tr>
 
 <?php
 }
 ?>
+
+</table>
+<?php } ?>
